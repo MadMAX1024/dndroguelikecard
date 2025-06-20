8abd7ba7ce5f78e8ce4218c6f2db3c74595c1fed
MadMax1024 <longjiaowang2016@gmail.com>
2025-06-20 16:07:20 -0400

    initial port and password for server

--- sql.js
+++ sql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2')
 var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "Root001!",
     database: "demo1"
 
 });
